e04c8e02eea6820f70690a205e5a52ab981c1fb1
ak220193 <mail2meak22frcrio@gmail.com>
2025-12-16 17:32:49 +0530

    Build Error FiX

--- aishwarya_arts_main/app/api/auth/[...nextauth]/route.js
+++ aishwarya_arts_main/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import connectDB from "../../../lib/db";
+import { connectDB } from "../../../lib/db";
 import User from "../../../lib/models/User";
 import bcrypt from "bcryptjs";
 
